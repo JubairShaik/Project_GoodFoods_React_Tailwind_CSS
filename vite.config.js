28ee19863f50ef9b1ee9c14649a3b5de032a8180
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-07-17 16:00:49 +0530

    debugg in marquee

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
-  build: {
-    rollupOptions: {
-      external: ['react-fast-marquee'],
-    },
-  }, // Closing the build object
-});
+  plugins: [react()]
+})
